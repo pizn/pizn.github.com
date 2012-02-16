b02a007145df765ebad21d757ef51dca8c38bcac
pizn <pizner@gmail.com>
2012-02-16 20:15:03 +0800

    test category

--- _plugins/category_plugin.rb
+++ _plugins/category_plugin.rb
@@ -8,26 +8,26 @@ module Jekyll
       @name = 'index.html'
 
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'category.html')
+      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
       self.data['category'] = category
-      self.data['title'] = "Archives for " + category + " Category"
-      self.data['keywords'] = category
-      self.data['description'] = "Archives for " + category + " Category."
+
+      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
+      self.data['title'] = "#{category_title_prefix}#{category}"
     end
   end
 
   class CategoryGenerator < Generator
     safe true
-
+    
     def generate(site)
-      if site.layouts.key? 'category'
-        dir = site.config['category_dir'] || 'category'
+      if site.layouts.key? 'category_index'
+        dir = site.config['category_dir'] || 'categories'
         site.categories.keys.each do |category|
           write_category_index(site, File.join(dir, category), category)
         end
       end
     end
-
+  
     def write_category_index(site, dir, category)
       index = CategoryIndex.new(site, site.source, dir, category)
       index.render(site.layouts, site.site_payload)
