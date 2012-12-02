6bba2a8e6470b1ad14518f15aba952fcc93634bd
pizn <pizner@gmail.com>
2012-12-02 11:19:29 +0800

    update

--- js/violet.js
+++ js/violet.js
@@ -6,18 +6,18 @@
 var violet = violet || {
     VERSION: '2.0',
     checkViolet: function() {
-    	var str = document.domain, rule = /^(www\.pizn\.me)?$/, url = "http://www.pizn.me", cookie_Key="__isViole__", cookie_value="Y";
-    	if(!rule.test(str)) {
-    		//document.cookie = cookie_key+'='+cookie_value+';expires='+new Date(new Date().getTime()+3600*24*30*1000).toUTCString();
-    		window.location.replace(url);
-    	}
-    	/**
-    	var isViolet = document.cookie.match(new RegExp(cookie_Key+ '=([Y])'))
-    	console.log(isViolet)
-    	if(isViolet != null && ) {
-			$("#J-tips").show();
-    	}
-    	**/
+        var str = document.domain, rule = /^(www\.pizn\.me)?$/, url = "http://www.pizn.me", cookie_Key="__isViole__", cookie_value="Y";
+        if(!rule.test(str)) {
+            //document.cookie = cookie_key+'='+cookie_value+';expires='+new Date(new Date().getTime()+3600*24*30*1000).toUTCString();
+            window.location.replace(url);
+        }
+        /**
+        var isViolet = document.cookie.match(new RegExp(cookie_Key+ '=([Y])'))
+        console.log(isViolet)
+        if(isViolet != null && ) {
+            $("#J-tips").show();
+        }
+        **/
     },
     showFlower: function(a, b){
         var banner = $('#J_banner');
@@ -75,76 +75,76 @@ var violet = violet || {
         }
     },
     highLightMenu: function() {
-    	var navs, url, cur, i;
-    	navs = $("#J_nav li a");
-		for(i = 1 ; i < navs.length; i++ ){
-			url = navs[i].href;
-			cur = window.location.href;
-			if(cur.indexOf(url) !=-1) {
-				navs[i].className = "current";
-				navs[0].className = "";
-			}
-			if(cur != navs[0].href) {
-				navs[0].className = "";
-			}
-		}     	
+        var navs, url, cur, i;
+        navs = $("#J_nav li a");
+        for(i = 1 ; i < navs.length; i++ ){
+            url = navs[i].href;
+            cur = window.location.href;
+            if(cur.indexOf(url) !=-1) {
+                navs[i].className = "current";
+                navs[0].className = "";
+            }
+            if(cur != navs[0].href) {
+                navs[0].className = "";
+            }
+        }       
     },
     getGitHub: function(name, num, id) {
-    	var user, url, loading, repo_name, repo_url, repo_desc, repo_watch, repo_fork, repo_len, i, meta, status, errmsg;
-    	user = name;
-    	url = "https://api.github.com/users/"+user+"/repos";
-    	loading = '<p class="v-loading"><img src="/images/loading.gif" alt="loading" /></p>';
-    	$(id).append(loading);
-    	$.ajax({
-    		url: url,
-    		dataType: "jsonp",
-    		success: function(data) {
-    			$(id).children("p").remove();
-    			repo_len = data.data.length;
-    			limit = data.meta["X-RateLimit-Remaining"];
-    			status = data.meta["status"];
-    			if(limit && limit > 0){
-       				if( repo_len < num ) {
-    					for( i = 0; i < repo_len; i++) {
-    						repo_name = data.data[i].name;
-    						repo_url = data.data[i].html_url;
-    						repo_desc = data.data[i].description;
-    						repo_watch = data.data[i].watchers;
-    						repo_fork = data.data[i].forks;
-    						project = '<article class="v-project-list">\
-                        			   <p class="name"><a href="' + repo_url + '" title="' + repo_name + '" target="_blank">' + repo_name + '</a>\
-                        			   <span class="v-project-watch">' + repo_watch + '</span>\
-                        			   <span class="v-project-forks">' + repo_fork + '</span></p>\
-                        			   <p class="descript">' + repo_desc + '</p>\
-                    				   </article>';
-                    		$(id).append(project);
-    					}	
-    				} else {
-    					for( i = 0; i < num; i++){
-    						repo_name = data.data[i].name;
-    						repo_url = data.data[i].html_url;
-    						repo_desc = data.data[i].description;
-    						repo_watch = data.data[i].watchers;
-    						repo_fork = data.data[i].forks;
-    						project = '<article class="v-project-list">\
-                        			   <p class="name"><a href="' + repo_url + '" title="' + repo_name + '" target="_blank">' + repo_name + '</a>\
-                        			   <span class="v-project-watch">' + repo_watch + '</span>\
-                        			   <span class="v-project-forks">' + repo_fork + '</span></p>\
-                        			   <p class="descript">' + repo_desc + '</p>\
-                    				   </article>';
-                    		$(id).append(project);
-    					}
-    					project = '<p class="v-more-right v-project-mor"><a href="https://github.com/' + user + '" target="_blank" title="PIZn on github"><span>&#10149;</span>All Projects</a></p>';
-    					$(id).append(project);
-    				}
-    			} else {
-    				errmsg = '<article class="v-project-normal"><p>通过项目的开发过程，能够得到更多的锻炼。</p>\
-    						  <p>在 GitHub 上有 PIZn 的几个小项目，例如 Alice, ColorCode, Blog 等等。</p></article>\
-    					      <p class="v-more-right"><a href="https://github.com/' + user + '" target="_blank" title="PIZn on github"><span>&#10149;</span>All Projects</a></p>';
-    				$(id).append(errmsg);
-    			}
-    		}
-    	});
+        var user, url, loading, repo_name, repo_url, repo_desc, repo_watch, repo_fork, repo_len, i, meta, status, errmsg;
+        user = name;
+        url = "https://api.github.com/users/"+user+"/repos";
+        loading = '<p class="v-loading"><img src="/images/loading.gif" alt="loading" /></p>';
+        $(id).append(loading);
+        $.ajax({
+            url: url,
+            dataType: "jsonp",
+            success: function(data) {
+                $(id).children("p").remove();
+                repo_len = data.data.length;
+                limit = data.meta["X-RateLimit-Remaining"];
+                status = data.meta["status"];
+                if(limit && limit > 0){
+                    if( repo_len < num ) {
+                        for( i = 0; i < repo_len; i++) {
+                            repo_name = data.data[i].name;
+                            repo_url = data.data[i].html_url;
+                            repo_desc = data.data[i].description;
+                            repo_watch = data.data[i].watchers;
+                            repo_fork = data.data[i].forks;
+                            project = '<article class="v-project-list">\
+                                       <p class="name"><a href="' + repo_url + '" title="' + repo_name + '" target="_blank">' + repo_name + '</a>\
+                                       <span class="v-project-watch">' + repo_watch + '</span>\
+                                       <span class="v-project-forks">' + repo_fork + '</span></p>\
+                                       <p class="descript">' + repo_desc + '</p>\
+                                       </article>';
+                            $(id).append(project);
+                        }   
+                    } else {
+                        for( i = 0; i < num; i++){
+                            repo_name = data.data[i].name;
+                            repo_url = data.data[i].html_url;
+                            repo_desc = data.data[i].description;
+                            repo_watch = data.data[i].watchers;
+                            repo_fork = data.data[i].forks;
+                            project = '<article class="v-project-list">\
+                                       <p class="name"><a href="' + repo_url + '" title="' + repo_name + '" target="_blank">' + repo_name + '</a>\
+                                       <span class="v-project-watch">' + repo_watch + '</span>\
+                                       <span class="v-project-forks">' + repo_fork + '</span></p>\
+                                       <p class="descript">' + repo_desc + '</p>\
+                                       </article>';
+                            $(id).append(project);
+                        }
+                        project = '<p class="v-more-right v-project-mor"><a href="https://github.com/' + user + '" target="_blank" title="PIZn on github"><span>&#10149;</span>All Projects</a></p>';
+                        $(id).append(project);
+                    }
+                } else {
+                    errmsg = '<article class="v-project-normal"><p>通过项目的开发过程，能够得到更多的锻炼。</p>\
+                              <p>在 GitHub 上有 PIZn 的几个小项目，例如 Alice, ColorCode, Blog 等等。</p></article>\
+                              <p class="v-more-right"><a href="https://github.com/' + user + '" target="_blank" title="PIZn on github"><span>&#10149;</span>All Projects</a></p>';
+                    $(id).append(errmsg);
+                }
+            }
+        });
     }
 }
 /**
@@ -200,8 +200,8 @@ violet.goTop = {
     }
 }
 $(document).ready(function(){
-	violet.checkViolet();
-  	//highLightMenu    
+    //violet.checkViolet();
+    //highLightMenu    
     violet.highLightMenu();
     violet.goTop.run();
     //selfPhoto
